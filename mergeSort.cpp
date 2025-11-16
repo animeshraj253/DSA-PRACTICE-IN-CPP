97b15e15503c21b68b9e0998de0258ea1db58acc
animeshraj253 <animeshraj253@gmail.com>
2025-11-17 03:07:37 +0530

    re commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,77 +1,87 @@
 #include<iostream>
 using namespace std;
 
-void merge(int *arr, int s, int e){
+void merge(int *arr, int s , int e){
 
-    int mid = (s+e) / 2;
+    int mid = s + (e-s)/2;
 
     int len1 = mid - s + 1;
-    int len2 = e-mid;
-    
+    int len2 = e - mid;
+
     int *first = new int[len1];
     int *second = new int[len2];
 
     // copy value
     int mainArrayIndex = s;
-    for(int i = 0; i<len1; i++){
-        first[i] = arr[mainArrayIndex++]; 
+    for (int i = 0; i<len1; i++){
+        first[i] = arr[mainArrayIndex];
+        mainArrayIndex++;
+    }
+    // not inportant
+    mainArrayIndex = mid+1;
+    for (int i = 0; i<len2; i++){
+        second[i] = arr[mainArrayIndex];
+        mainArrayIndex++;
     }
 
-    mainArrayIndex = mid + 1;
-    for( int i = 0 ; i<len2; i++){
-        second[i] = arr[mainArrayIndex++];
-    }
-
-    int index1 = 0;
-    int index2 = 0;
+    // merge 2 sort
+    int inx1 = 0;
+    int inx2 = 0;
     mainArrayIndex = s;
 
-    while(index1 < len1 && index2<len2){
-        if(first[index1] < second[index2]){
-            arr[mainArrayIndex++] = first[index1++];
+    while(inx1 < len1 && inx2 < len2 ){
+        if(first[inx1] <= second[inx2]){
+            arr[mainArrayIndex] = first[inx1];
+            mainArrayIndex++;
+            inx1++;
         }
         else{
-            arr[mainArrayIndex++] = second[index2++];
+            arr[mainArrayIndex] = second[inx2];
+            mainArrayIndex++;
+            inx2++;
         }
     }
-
-    while(index1 < len1){
-        arr[mainArrayIndex++] = first[index1++];
+    while(inx1 < len1){
+        arr[mainArrayIndex] = first[inx1];
+        mainArrayIndex++;
+        inx1++;
+    }
+    while(inx2<len2){
+        arr[mainArrayIndex] = second[inx2];
+        mainArrayIndex++;
+        inx2++;
     }
 
-    while(index2 < len2){
-        arr[mainArrayIndex++] = second[index2++];
-    }
-
-    delete []first;
-    delete []second;
-
+    delete[] first;
+    delete[] second;
 }
 
-void mergeSort(int *arr, int s, int e){
-    if (s>=e)
+void mergeSort(int arr[],int s, int e){
+    if(s>=e){
         return;
-    int mid = (s + e) / 2 ;
+    }
+    int mid = s + (e-s)/2;
+
     // left part sort krna hai
     mergeSort(arr,s,mid);
 
-    // right partr sort krna hai
+    // right part sort krna hai
     mergeSort(arr,mid+1,e);
 
-    // merge krna hai
+    //
     merge(arr,s,e);
-}
+}   
 
 int main(){
-
-    int arr[] = {9,3,2,6,7};
+    int arr[] = {2,5,1,6,9};
     int n = 5;
+    
     mergeSort(arr,0,n-1);
-
-    for ( int i = 0; i<n ; i++){
-        cout<<arr[i] << " ";
+    
+    for(int i : arr){
+        cout<< i << " ";
     }
-    cout<<endl<<endl;
+    cout<<endl;
 
     return 0;
 }
